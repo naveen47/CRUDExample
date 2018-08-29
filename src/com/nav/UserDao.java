2fb8b5bd2d70c790a483ed9fa76e1ad9fc99c2ad
Naveen <naveen.poojary47@gmail.com>
2018-08-29 10:24:59 +0530

    Initial commit

---
+++ src/com/nav/UserDao.java
@@ -0,0 +1,106 @@
+package com.nav;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao {
+	private Connection connection;
+
+    public UserDao() {
+        connection = DbUtil.getConnection();
+    }
+
+    public void addUser(User user) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("insert into users(name,password,email,country) values (?, ?, ?, ? )");
+            // Parameters start with 1
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, user.getCountry());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteUser(int userId) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("delete from users where userid=?");
+            // Parameters start with 1
+            preparedStatement.setInt(1, userId);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUser(User user) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("update users set name=?, password=?, email=?, country=?" +
+                            "where userid=?");
+            // Parameters start with 1
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, user.getCountry());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<User>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from users");
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("userid"));
+                user.setName(rs.getString("firstname"));
+                user.setEmail(rs.getString("lastname"));
+                user.setCountry(rs.getString("dob"));
+                //user.setEmail(rs.getString("email"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    public User getUserById(int userId) {
+        User user = new User();
+        try {
+            PreparedStatement preparedStatement = connection.
+                    prepareStatement("select * from users where userid=?");
+            preparedStatement.setInt(1, userId);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            if (rs.next()) {
+            	  user.setId(rs.getInt("userid"));
+                  user.setName(rs.getString("firstname"));
+                  user.setEmail(rs.getString("lastname"));
+                  user.setCountry(rs.getString("dob"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+}
