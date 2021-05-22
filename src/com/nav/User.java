399348dceb5dd401462be2a99198163cb7f60d8e
naveennp <naveen.poojary47@gmail.com>
2021-05-22 18:42:35 +0530

    updated model

--- src/com/nav/User.java
+++ src/com/nav/User.java
@@ -9,7 +9,7 @@ public class User {
 	private String age;
 	private String address;
  	private String address;
- 	private Date date;
+
 
 	public int getId() {
 		return id;
@@ -54,10 +54,4 @@ public class User {
 		this.age = age;
 	}
 
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
 }
